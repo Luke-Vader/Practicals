161a79be67da39043dcf077b87c3d8657641a389
Luke-Vader <32070588+Luke-Vader@users.noreply.github.com>
2019-06-25 08:47:54 +0530

    Update commentsOnGui-rishabh.py
    stunt machare

--- Artificial Intelligence/commentsOnGui-rishabh.py
+++ Artificial Intelligence/commentsOnGui-rishabh.py
@@ -9,3 +9,27 @@ window = Tk()
 
 #START is the window title now
 window.title("START")
+
+frame = Frame(window)
+
+label = Label(frame, text = "Give me an Object")
+entry = Entry(frame)
+
+def dialog():
+    box.showinfo('Open', 'Shit '+entry.get())
+
+btn = Button(frame, text = "Click Me", command = dialog)
+
+'''
+label.pack(side = LEFT)
+entry.pack(side = RIGHT)
+btn.pack(side = BOTTOM)
+'''
+
+label.grid(row = 0)
+entry.grid(row = 0, column = 1)
+btn.grid(row = 1, column = 3)
+
+frame.pack(padx = 40, pady = 50)
+
+window.mainloop()
