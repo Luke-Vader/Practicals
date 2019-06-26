697af8e308cb8a899fea7a60720afa57491037c7
Luke-Vader <rishabhbanerjee2000@gmail.com>
2019-06-26 08:48:16 +0530

    maar do koi
    Co-Authored-By: surajkumardas23 <surajkumardas23@users.noreply.github.com>

---
+++ C#/rishabh_C#/ConsoleApplication6/OROL.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication6
+{
+    class A
+    {
+        public virtual void Test()
+        {
+            Console.WriteLine("A:Test()");
+        }
+    }
+
+    class B : A
+    {
+        public override void Test()
+        {
+            Console.WriteLine("B:Test()");
+        }
+    }
+
+    class C : B
+    {
+        public override void Test()
+        {
+            Console.WriteLine("C:Test()");
+        }
+    }
+
+    class OROL : C
+    {
+        static void Main(string[] s)
+        {
+
+            Console.ReadKey();
+        }
+    }
+}
