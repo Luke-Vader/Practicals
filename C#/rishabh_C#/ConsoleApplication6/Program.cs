145594ffc07d21b4dc79b281681f06880dd24c5c
Luke-Vader <rishabhbanerjee2000@gmail.com>
2019-06-26 08:29:27 +0530

    hua kuch toh

---
+++ C#/rishabh_C#/ConsoleApplication6/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication6
+{
+    class A {
+        public A() : this("welcome") {
+            Console.WriteLine("in A()");
+        }
+
+        public A(string s)
+        {
+            Console.WriteLine("in A(string)");
+        }
+    }
+
+    class B : A
+    {
+        public B()
+        {
+            Console.WriteLine("in B()");
+        }
+
+        public B(string s)
+        {
+            Console.WriteLine("in B(string)");
+        }
+    }
+
+    class Program : B
+    {
+        Program(string s)
+        {
+            Console.WriteLine("inside Program(string)");
+        }
+
+        Program()
+        {
+            Console.WriteLine("inside Program()");        
+        }
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+
+            Console.ReadKey();
+        }
+    }
+}
