e380157d56901d7df1f1340a676df56bb7cd5c36
Luke-Vader <rishabhbanerjee2000@gmail.com>
2019-07-04 09:23:55 +0530

    kachra karre idhar baihtke
    Co-Authored-By: janak bohra <janakbohra29@gmail.com>
    Co-Authored-By: luke-vader <luke-vader@users.noreply.github.com>
    Co-Authored-By: Monesh Chandra Maurya <moneshnj@users.noreply.github.com>
    Co-Authored-By: surajkumardas23 <surajkumardas23@users.noreply.github.com>

--- Data Mining/KNN-rishabh.R
+++ Data Mining/KNN-rishabh.R
@@ -2,3 +2,13 @@ a = iris[1:5,]
 b = iris[51:55,]
 c = iris[101:104,]
 TData = rbind(a,b,c)
+
+TData = as.data.frame(scale(iris[,-5]))
+TData$class = iris$species
+install.packages("caret")
+install.packages("e1071")
+library(caret)
+library(e1071)
+model = train (class~.,data = TData,method = "knn")
+t = data
+predict(model,t)
