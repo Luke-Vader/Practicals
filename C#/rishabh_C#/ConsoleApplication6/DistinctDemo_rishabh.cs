3357db18c70e8fd445aef4f3f9f7ee37959452f7
Luke-Vader <rishabhbanerjee2000@gmail.com>
2019-07-10 08:43:58 +0530

    ho gaya
    Co-Authored-By: luke-vader <luke-vader@users.noreply.github.com>
    Co-Authored-By: surajkumardas23 <surajkumardas23@users.noreply.github.com>

---
+++ C#/rishabh_C#/ConsoleApplication6/DistinctDemo_rishabh.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication6
+{
+    class DistinctDemo_rishabh
+    {
+        IList<int> numbers = new List<int>() {
+            1,1,1,2,2,2,3,3,3
+        };
+
+        var distinctNumbers = numbers.Distinct().ToList();
+        distinctNumbers.ForEach(x => Console.WriteLine(x));
+        Console.ReadKey();
+
+    }
+}
