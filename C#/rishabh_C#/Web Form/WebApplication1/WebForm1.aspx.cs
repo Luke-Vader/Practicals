0fb25ab9aee011c6d6c3c5d14f954a9dd9782415
Luke-Vader <rishabhbanerjee2000@gmail.com>
2019-08-07 10:03:32 +0530

    this is the real deal
    Co-Authored-By: Elton <dsilvaelton32@users.noreply.github.com>
    Co-Authored-By: janak bohra <janakbohra29@gmail.com>
    Co-Authored-By: luke-vader <luke-vader@users.noreply.github.com>
    Co-Authored-By: Monesh Chandra Maurya <moneshnj@users.noreply.github.com>
    Co-Authored-By: surajkumardas23 <surajkumardas23@users.noreply.github.com>

---
+++ C#/rishabh_C#/Web Form/WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection sql = new SqlConnection(@"Data Source=DESKTOP-R031P94\SQLEXPRESS;Integrated Security=True");
+            sql.Open();
+            string sqlQuery = "insert into employee_details values(@t1,@t2,@t3,@t4)";
+            SqlCommand command = new SqlCommand(sqlQuery, sql);
+            command.Parameters.AddWithValue("@t1", int.Parse(TextBox1.Text));
+            command.Parameters.AddWithValue("@t2", TextBox2.Text);
+            command.Parameters.AddWithValue("@t3", int.Parse(TextBox3.Text));
+            command.Parameters.AddWithValue("@t4", TextBox4.Text);
+            command.ExecuteNonQuery();
+            sql.Close();
+
+        }
+    }
+}
