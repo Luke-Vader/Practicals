597ed8e8b8ceaad7a10c98fcdb71868c2488cd5f
Luke-Vader <32070588+Luke-Vader@users.noreply.github.com>
2019-09-11 09:19:26 +0530

    Update eXeMeLpeLINQ.cs

--- C#/rishabh_C#/eXeMeLpeLINQ.cs
+++ C#/rishabh_C#/eXeMeLpeLINQ.cs
@@ -14,13 +14,26 @@ namespace ConsoleApplication3
             XDocument booksCatalog = XDocument.Load("C:/Users/windows/Documents/Visual Studio 2008/Projects/ConsoleApplication3/ConsoleApplication3/books.xml");
             IEnumerable<string> newBooks = from x in booksCatalog.Element("catalog").Elements("book")
                      where DateTime.ParseExact((string)x.Element("publish_date"), "yyyy-mm-dd", null) >
-                     DateTime.ParseExact("2000-01-01", "yyyy-mm-dd", null)
+                     DateTime.ParseExact("2001-01-01", "yyyy-mm-dd", null)
                      let nm = (string)x.Element("title")
                      select nm;
+            Console.WriteLine("Books published After 2001-01-01");
             foreach(string m in newBooks)
             {
                 Console.WriteLine(m);
             }
+
+            IEnumerable<string> oldBooks = from x in booksCatalog.Element("catalog").Elements("book")
+                                           where DateTime.ParseExact((string)x.Element("publish_date"), "yyyy-mm-dd", null) <
+                                           DateTime.ParseExact("2001-01-01", "yyyy-mm-dd", null)
+                                           let nm = (string)x.Element("title")
+                                           select nm;
+            Console.WriteLine("Books published Before 2001-01-01");
+            foreach (string m in oldBooks)
+            {
+                Console.WriteLine(m);
+            }
+
             Console.ReadKey();
 
         }
