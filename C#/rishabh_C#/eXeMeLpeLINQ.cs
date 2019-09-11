19000b90c36ecd4fae5823f54e45430e919b506d
Luke-Vader <32070588+Luke-Vader@users.noreply.github.com>
2019-09-11 09:13:17 +0530

    Create eXeMeLpeLINQ.cs

---
+++ C#/rishabh_C#/eXeMeLpeLINQ.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I\'m gonna do shit now");
+            XDocument booksCatalog = XDocument.Load("C:/Users/windows/Documents/Visual Studio 2008/Projects/ConsoleApplication3/ConsoleApplication3/books.xml");
+            IEnumerable<string> newBooks = from x in booksCatalog.Element("catalog").Elements("book")
+                     where DateTime.ParseExact((string)x.Element("publish_date"), "yyyy-mm-dd", null) >
+                     DateTime.ParseExact("2000-01-01", "yyyy-mm-dd", null)
+                     let nm = (string)x.Element("title")
+                     select nm;
+            foreach(string m in newBooks)
+            {
+                Console.WriteLine(m);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
